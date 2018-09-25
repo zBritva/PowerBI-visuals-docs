42221749aca861bcf6240584411f5742148b1885
Nikita Grachev <v-grniki@microsoft.com>
2018-09-25 17:36:53 +0700

    Completed issues on Github

--- assets/js/github-comments.js
+++ assets/js/github-comments.js
@@ -1,57 +1,86 @@
-// Original code taken with permission from : https://github.com/dwilliamson/donw.io/blob/master/public/js/github-comments.js
+function loadComments(comments) {
 
-// use of ajax vs getJSON for headers use to get markdown (body vs body_html)
+    const items = comments.map(function (item) {
+        const posted_at = (new Date(item.updated_at)).toLocaleString();
+        return `
+        <div class="js-timeline-item js-timeline-progressive-focus-container">
+            <div class="timeline-comment-wrapper js-comment-container">
+                <div class="avatar-parent-child timeline-comment-avatar">
+                    <a class="d-inline-block" data-octo-click="hovercard-link-click" aria-describedby="hovercard-aria-description">
+                        <img class="avatar rounded-1" height="44" width="44" alt="@${item.user.login}" src="${item.user.avatar_url}"></a>
 
-function ShowComments(repo_name, comment_id, page_id) {
-    $.ajax({
-        url: "https://api.github.com/repos/" + repo_name + "/issues/" + comment_id + "/comments" + "?page=" + page_id,
-        headers: { Accept: "application/vnd.github.v3.html+json" },
-        dataType: "json",
-        success: function (comments, textStatus, jqXHR) {
+                </div>
 
-            if (1 == page_id) {
-                // post button 
-                var url = "https://github.com/" + repo_name + "/issues/" + comment_id + "#new_comment_field";
-                $("#gh-comments-list").append("<form action='" + url + "' rel='nofollow'> <input type='submit' value='Post a comment on Github' /> </form>");
-            }
+                <div class="timeline-comment-group js-minimizable-comment-group">
+    
+                    <div class="unminimized-comment editable-comment comment previewable-edit js-comment js-task-list-container timeline-comment reorderable-task-lists current-user">
+   
+                        <div class="timeline-comment-header clearfix">
+                            <h3 class="timeline-comment-header-text f5 text-normal">
+                                <strong class="css-truncate">
+                                    <a class="author text-inherit css-truncate-target" data-octo-click="hovercard-link-click" data-octo-dimensions="link_type:self" href="${item.user.html_url}" aria-describedby="hovercard-aria-description">${item.user.login}</a>
+                                  </strong>
 
-            // Individual comments
-            $.each(comments, function (i, comment) {
+                                commented
+                                <a href="${item.html_url}" class="timestamp"><relative-time datetime="${item.updated_at}" title="${posted_at}">${posted_at}</relative-time></a>
+                            </h3>
+                        </div>
 
-                var date = new Date(comment.created_at);
 
-                var t = "<div id='gh-comment'>";
-                t += "<img src='" + comment.user.avatar_url + "' width='24px'>";
-                t += "<b><a href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>";
-                t += " posted at ";
-                t += "<em>" + date.toUTCString() + "</em>";
-                t += "<div id='gh-comment-hr'></div>";
-                t += comment.body_html;
-                t += "</div>";
-                $("#gh-comments-list").append(t);
-            });
+                    <div class="edit-comment-hide">
+                    
+                        <task-lists sortable="">
+                            <table class="d-block">
+                                <tbody class="d-block">
+                                    <tr class="d-block">
+                                        <td class="d-block comment-body markdown-body js-comment-body">
+                                        ${item.body_html}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </task-lists>
+                    </div>
+            </div>
 
-            // Call recursively if there are more pages to display
-            var linksResponse = jqXHR.getResponseHeader("Link");
-            if (linksResponse) {
-                var entries = linksResponse.split(",");
-                for (var j = 0; j < entries.length; j++) {
-                    var entry = entries[j];
-                    if ("next" == entry.match(/rel="([^"]*)/)[1]) {
-                        ShowComments(repo_name, comment_id, page_id + 1);
-                        break;
-                    }
-                }
-            }
-        },
-        error: function () {
-            $("#gh-comments-list").append("Comments are not open for this post yet.");
+        </div>
+    `
+    });
+
+    let placeholder = document.getElementById("gh-comments-list");
+    if (placeholder) {
+        placeholder.innerHTML = items.join();
+    }
+}
+
+
+function initGithubComments() {
+    const ghComments = document.getElementById("gh-comments");
+
+    if (!ghComments) {
+        return false;
+    }
+    const repo_name = ghComments.getAttribute("data-repository");
+    const comment_id = ghComments.getAttribute("data-issue-id");
+
+    if (!repo_name || !comment_id) {
+        return false;
+    }
+
+    const page_id = 1;
+
+    const url = `https://api.github.com/repos/${repo_name}/issues/${comment_id}/comments?page=${page_id}`;
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/vnd.github.v3.html+json'
         }
-    });
+    }).then(res => res.json())
+        .then(response => loadComments(response))
+        .catch(error => console.error('Error:', error));
+
 }
 
-function DoGithubComments(repo_name, comment_id) {
-    $(document).ready(function () {
-        ShowComments(repo_name, comment_id, 1);
-    });
-}
+initGithubComments();
